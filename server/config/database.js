cbab911e678539fd2b62a427fd8460a74990a634
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-10-30 23:05:19 +0530

    updated

---
+++ server/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+exports.connect = () => {
+ mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+ }).then(() => {
+    console.log('Successfully connected to the database');
+ }).catch(err => {
+        console.log('Could not connect to the database. Exiting now...', err);
+        process.exit();
+    });
+}
