44e123df9ab4e64dc37a7e4e826c472ca20c24b2
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-05 16:00:32 +0530

    Add Cloudinary configuration to server

---
+++ server/config/cloudinary.js
@@ -0,0 +1,14 @@
+const cludinary = require('cloudinary').v2;
+
+exports.cloudinaryConnect = () => {
+
+    try {
+        cludinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_SECRET_KEY
+        });
+    } catch (error) {
+        console.log("error", error);
+    }
+}
