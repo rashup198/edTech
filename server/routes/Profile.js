b16c128ef5367e0767f2c33d8162056541c7c077
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-05 23:40:43 +0530

    Add Profile routes for user authentication and profile management

---
+++ server/routes/Profile.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const {auth}= require('../middlewares/auth');
+
+// Controllers
+const{getEnrolledCourses,updateDisplayPicture,getAllUserDetails,deleteProfile,updateProfile,} = require('../controllers/Profile');
+
+// Profile routes
+
+// delete profile
+router.delete('/deleteProfile',auth,deleteProfile);
+// update profile
+router.put('/updateProfile',auth,updateProfile);
+// get all user details
+router.get('/getUserDetails',auth,getAllUserDetails);
+// get enrolled courses
+router.get('/getEnrolledCourses',auth,getEnrolledCourses);
+// update display picture
+router.put('/updateDisplayPicture',auth,updateDisplayPicture);
+
+module.exports = router;
+s
