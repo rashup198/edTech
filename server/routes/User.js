dab2cc647a02610b47b798f7177b999f6e83ed2e
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-05 23:45:56 +0530

    Add routes for login, signup, OTP, change password, reset password and reset password token

--- server/routes/User.js
+++ server/routes/User.js
@@ -10,3 +10,23 @@ const {resetPassword,resetPasswordToken}= require('../controllers/ResetPassword'
 
 const {auth, isInstrutor,isAdmin,isStudent} = require('../middlewares/auth');
 
+//routes for login and signup and authentication
+
+// user login
+router.post('/login',login);
+// user signup
+router.post('/signup',signUp);
+//sending otp to email
+router.post('/sendOTP',sendOTP);
+//change password
+router.post('/changepassword',changePassword);
+
+
+//reset password
+//reset password token
+router.post('/resetPasswordToken',resetPasswordToken);
+//reset password after verifying token
+router.post('/resetPassword',resetPassword);
+
+module.exports = router;
+
