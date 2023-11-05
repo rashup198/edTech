a9d07dc2353b0c8c5d5ea328f6f4098b4fb4aee2
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-05 13:23:40 +0530

    Fix category controller functions and add category page details

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -1,6 +1,6 @@
 const Category = require('../models/category');
 
-//create tag
+//create category
 
 exports.createCategory = async (req, res) => {
     try {
@@ -38,7 +38,7 @@ exports.createCategory = async (req, res) => {
 
 }
 
-// get all tags
+// get all category
 
 exports.getallCategory = async (req, res) => {
 
@@ -57,5 +57,49 @@ exports.getallCategory = async (req, res) => {
             success: false,
             message:error.message
         }); 
+    }
+}
+
+// get category details
+
+exports.categoryPageDetails = async (req,res)=>{
+    try {
+        // get category id
+        const {categoryId} = req.body;
+        //get courses for that category
+        const selectedCategory = await Category.findById(categoryId).populate("courses").exec();
+        //validate category
+        if(!selectedCategory){
+            return res.status(404).json({
+                success:false,
+                message:"Category not found"
+            })
+        }
+        //get coursed for different category
+        const differentCategories = await Category.find({_id:{$ne:categoryId}}).populate("courses").exec(); 
+
+        //get top selling courses
+        const topSellingCourses = await Course.find({}).sort({numberOfStudentsEnrolled:("desc")}).limit(10).exec();
+        //get top rated courses
+
+        const topRatedCourses = await Course.find({}).sort({averageRating:("desc")}).limit(10).exec();
+
+        //send response
+
+        return res.status(200).json({
+            success:true,
+            message:"Category page details fetched successfully",
+            selectedCategory,
+            differentCategories,
+            topSellingCourses,
+            topRatedCourses
+        })
+
+    } catch (error) {
+        console.log("error",error);
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })    
     }
 }
