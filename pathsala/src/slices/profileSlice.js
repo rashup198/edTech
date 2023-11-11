7cbd1bc8c16d094d2c2f288f9d37328c7259a102
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-12 01:03:53 +0530

    Refactor profileSlice.js to use createSlice from "@reduxjs/toolkit"

--- pathsala/src/slices/profileSlice.js
+++ pathsala/src/slices/profileSlice.js
@@ -1,24 +1,18 @@
-import {createSlice} from '@reduxjs/toolkit';
-
-//initial state
+import {createSlice} from "@reduxjs/toolkit"
 
 const initialState = {
-    user:null
-}
+    user:null,
+};
 
-//create slice
 const profileSlice = createSlice({
     name:"profile",
-    initialState,
-    reducers:{
-        setUser(state, value)
-        {
-            state.token=value.payload;
-        }
-    }
-})  
-
-//export actions
+    initialState: initialState,
+    reducers: {
+        setUser(state, value) {
+            state.user = value.payload;
+        },
+    },
+});
 
 export const {setUser} = profileSlice.actions;
 export default profileSlice.reducer;
