520970f6fc333eb0c8dd2b24c517725fa5ad0504
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-11 12:32:38 +0530

    Add sublinks to Catalog dropdown in Navbar component

--- pathsala/src/components/common/Navbar.jsx
+++ pathsala/src/components/common/Navbar.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link, matchPath } from 'react-router-dom'
 import { NavbarLinks } from '../../data/navbar-links'
 import { useLocation } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 import ProfileDropDown from '../core/auth/ProfileDropDown'
+import { apiConnector } from '../../services/apiconnector'
+import { categories } from '../../services/api'
+import { IoIosArrowDropdown } from 'react-icons/io'
 
+const subLinks = [
+  {
+    title: "Web Development",
+    link:"catalog/web-development"
+  },
+  {
+    title: "App Development",
+    link:"catalog/app-development"
+  }
+]
 const Navbar = () => {
 
   const {token} = useSelector(state => state.auth);
   // const {user} = useSelector(state => state.user);
   const {totalItems}= useSelector(state => state.cart);
+
+  // const [subLinks, setSubLinks] = React.useState([]);
+
+//   const fetchSubLinks = async () => 
+//   {
+//   try {
+//     const result =await apiConnector("GET", categories.CATEGORIES_API);
+//     setSubLinks(result.data);
+//     console.log("categories list", result );
+//   } catch (error) {
+//     console.log("cannot fetch the categories list");
+//   }
+// }
+ 
+  // useEffect(() => { 
+  //   fetchSubLinks();
+  // },[])
 
   const location = useLocation();
 
@@ -24,20 +54,29 @@ const Navbar = () => {
 
         {/* // Navbar Links */}
         <nav>
-          <ul className='flex gap-x-6 text-richblack-25'>
+          <ul className='flex gap-x-6 group text-richblack-25'>
             {NavbarLinks.map((link,index) => (
               <li key={index}>
                 {
-                  link.title ==="Catalog" ? (<div></div>) : (<div>
+                  link.title ==="Catalog" ? (
+                  <div className='relative flex gap-3 items-center group'>
+                    <p>{link.title}</p>
+                     <IoIosArrowDropdown className='text-white' /> 
+                     <div className='invisible absolute left-[10%] top-[90%] flex flex-col rounded-md bg-richblack-5 text-richblack-900 opacity-0 transition-all duration-200 group-hover:visible group-hover:opacity-100 lg:w-[180px] gap-2 '>
+                     {subLinks.map((subLink,index) => (
+                        <Link key={index} to={`${subLink.link}`} >
+                         {subLink.title}
+                        </Link>
+                      ))}
+                     </div> 
+                  </div>) : (<div>
                     <Link to={link?.path}>
                       <div className={` ${matchRoute(link?.path) ? 'text-yellow-25 hover:text-yellow-25' : 'text-white'}`}>
                         {link?.title}
                       </div>
                     </Link>
-
                   </div>)
                 }
-                
               </li>
             ))}
             
