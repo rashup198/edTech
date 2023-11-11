777961c0cce62ec2b81a227d65fa7e31d1278a03
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-12 01:03:11 +0530

    Add useOnClickOutside hook and Error page component

---
+++ pathsala/src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='flex justify-center items-center text-3xl text-caribbeangreen-300'>
+      Error -404 Page Not Found
+
+    </div>
+  )
+}
+
+export default Error
