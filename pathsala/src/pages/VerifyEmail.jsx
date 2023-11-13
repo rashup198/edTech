2dfd5be1293ded75d1445f81b4c66e4c259d7092
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-14 00:17:28 +0530

    Refactor VerifyEmail component

--- pathsala/src/pages/VerifyEmail.jsx
+++ pathsala/src/pages/VerifyEmail.jsx
@@ -1,25 +1,19 @@
-import { useEffect, useState } from "react";
-import OtpInput from "react-otp-input";
-import { Link } from "react-router-dom";
-import { BiArrowBack } from "react-icons/bi";
-import { RxCountdownTimer } from "react-icons/rx";
-import { useDispatch, useSelector } from "react-redux";
-import { sendOtp, signUp } from "../services/operations/authAPI";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { BiArrowBack } from 'react-icons/bi';
+import { sendOtp, signUp } from '../services/operations/authAPI';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import OtpInput from 'react-otp-input';
 
-function VerifyEmail() {
-  const [otp, setOtp] = useState("");
-  const { signupData, loading } = useSelector((state) => state.auth);
+const VerifyEmail = () => {
+  const [otp, setOtp] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { signupData, loading } = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    // Only allow access of this route when user has filled the signup form
-    if (!signupData) {
-      navigate("/signup");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  
 
   const handleVerifyAndSignup = (e) => {
     e.preventDefault();
@@ -46,65 +40,50 @@ function VerifyEmail() {
     );
   };
 
+
   return (
-    <div className="min-h-[calc(100vh-3.5rem)] grid place-items-center">
+    <div className=" min-h-screen flex items-center justify-center">
       {loading ? (
         <div>
-          <div className="spinner"></div>
+          <h1>Loading......</h1>
         </div>
       ) : (
-        <div className="max-w-[500px] p-4 lg:p-8">
-          <h1 className="text-richblack-5 font-semibold text-[1.875rem] leading-[2.375rem]">
-            Verify Email
-          </h1>
-          <p className="text-[1.125rem] leading-[1.625rem] my-4 text-richblack-100">
-            A verification code has been sent to you. Enter the code below
-          </p>
-          <form onSubmit={handleVerifyAndSignup}>
+        <div className="bg-white p-8 rounded shadow-md w-96">
+          <h1 className="text-2xl font-bold mb-4">Verify Email</h1>
+          <p className="text-gray-600 mb-4">Enter the verification code we just sent to your email address</p>
+          <form onSubmit={handleOnSubmit} className="space-y-4">
             <OtpInput
-              value={otp}
-              onChange={setOtp}
-              numInputs={6}
-              renderInput={(props) => (
-                <input
-                  {...props}
-                  placeholder="-"
-                  style={{
-                    boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
-                  }}
-                  className="w-[48px] lg:w-[60px] border-0 bg-richblack-800 rounded-[0.5rem] text-richblack-5 aspect-square text-center focus:border-0 focus:outline-2 focus:outline-yellow-50"
-                />
-              )}
-              containerStyle={{
-                justifyContent: "space-between",
-                gap: "0 6px",
-              }}
+            value={otp}
+            onChange={setOtp}
+            numInputs={6}
+            renderSeparator={<span>-</span>}
+            renderInput={(props) => <input {...props} className='border border-black' />}
             />
+
             <button
               type="submit"
-              className="w-full bg-yellow-50 py-[12px] px-[12px] rounded-[8px] mt-6 font-medium text-richblack-900"
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
             >
-              Verify Email
+              Verify
             </button>
           </form>
           <div className="mt-6 flex items-center justify-between">
-            <Link to="/signup">
-              <p className="text-richblack-5 flex items-center gap-x-2">
-                <BiArrowBack /> Back To Signup
+            <Link to="/login" className="text-blue-500">
+              <p className="flex items-center gap-x-2">
+                <BiArrowBack /> Back To Login
               </p>
             </Link>
-            <button
-              className="flex items-center text-blue-100 gap-x-2"
-              onClick={() => dispatch(sendOtp(signupData.email))}
-            >
-              <RxCountdownTimer />
-              Resend it
-            </button>
           </div>
+          <button
+            onClick={() => dispatch(sendOtp(signupData.email))}
+            className="text-blue-500 hover:underline focus:outline-none"
+          >
+            Resend it
+          </button>
         </div>
       )}
     </div>
   );
-}
+};
 
 export default VerifyEmail;
