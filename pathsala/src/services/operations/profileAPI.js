4264f9298a1e094560ff91e094b28ddc9ad71c5c
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-16 11:05:47 +0530

    Add RenderTotalAmount component and fix import paths in SettingsAPI and profileAPI

--- pathsala/src/services/operations/profileAPI.js
+++ pathsala/src/services/operations/profileAPI.js
@@ -1,5 +1,5 @@
 import { toast } from "react-hot-toast"
-import { apiConnector } from "../apiConnector"
+import { apiConnector } from "../apiconnector"
 import { profileEndpoints } from "../api"
 import { logout } from "./authAPI"
 import { setLoading } from "../../slices/profileSlice";
