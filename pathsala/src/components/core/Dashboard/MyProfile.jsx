b0ba6f42b1ee72eb9239dfea910cfae1251a8344
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-16 11:05:32 +0530

    Update MyProfile component layout

--- pathsala/src/components/core/Dashboard/MyProfile.jsx
+++ pathsala/src/components/core/Dashboard/MyProfile.jsx
@@ -37,8 +37,9 @@ export default function MyProfile() {
           <RiEditBoxLine />
         </IconBtn>
       </div>
-      <div className="my-10 flex flex-col gap-y-10 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12">
-        <div className="flex w-full items-center justify-between">
+      <div className="w-[70vw] overflow-hidden">
+      <div className="my-10 w-[50vw] flex flex-col gap-y-10 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12 ">
+        <div className="flex items-center justify-between">
           <p className="text-lg font-semibold text-richblack-5">About</p>
           <IconBtn
             text="Edit"
@@ -59,7 +60,7 @@ export default function MyProfile() {
           {user?.additionalDetails?.about ?? "Write Something About Yourself"}
         </p>
       </div>
-      <div className="my-10 flex flex-col gap-y-10 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12">
+      <div className="my-10 w-[60vw] flex flex-col gap-y-10 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12">
         <div className="flex w-full items-center justify-between">
           <p className="text-lg font-semibold text-richblack-5">
             Personal Details
@@ -116,6 +117,7 @@ export default function MyProfile() {
             </div>
           </div>
         </div>
+      </div>
       </div>
     </>
   )
