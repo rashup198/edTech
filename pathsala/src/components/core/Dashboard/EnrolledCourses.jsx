ed5eb55735b87de06539892437fe8f748bb6f5e7
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-16 09:33:13 +0530

    Added EnrolledCourses component

---
+++ pathsala/src/components/core/Dashboard/EnrolledCourses.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const EnrolledCourses = () => {
+
+    
+
+  return (
+    <div>
+      <div className="">Enrolled Courses</div>
+    </div>
+  )
+}
+
+export default EnrolledCourses
